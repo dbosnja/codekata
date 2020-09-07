74fbc9a10fc7c0f23bfbd13de9d1891332be3699
dbosnja <dombosnjak@gmail.com>
2020-09-07 23:58:21 +0200

    helpers module created; kata02 script created

---
+++ kata02/kata02.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8  -*-
+
+from helpers import time_me
+
+
+def binary_chop(int_target, int_list):
+	""" does a binary search on a list and 
+		returns index of targeted_element 
+		from the list or -1 if not found
+
+	:rtype: int"""
+	pass	        
+
