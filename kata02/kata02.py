5d192d790e586a4171bbc330a92e9a77666864df
dbosnja <dombosnjak@gmail.com>
2020-09-15 21:06:42 +0200

    3. approach:    binary_chop - recursive edition - indices carry-on

--- kata02/kata02.py
+++ kata02/kata02.py
@@ -31,7 +31,7 @@ def binary_chop(int_target, int_list):
 			start_index = arithmetic_mean + 1  # O(1)
 		elif list_middle_value > int_target:
 			end_index = arithmetic_mean - 1  # O(1)
-	#  start_index == end_index True. check for 2-dim lists case
+	#  start_index == end_index True. 2 and 3 dim lists cases
 	if int_list[start_index] == int_target:  # O(1)
 		return start_index
 	return not_found_value	        
@@ -40,18 +40,18 @@ def binary_chop(int_target, int_list):
 
 #print(binary_chop(55, 'a_list'))
 #print(binary_chop(55, []))
-print(binary_chop(121, [i for i in xrange(1, 2 ** 20)]))
+#print(binary_chop(999, [i for i in xrange(1, 2 ** 20)]))
 #print(binary_chop(7, [1, 3, 5, 7]))
 
 
 # 2. approach:    binary_chop - recursive edition
 @time_me
-def binary_chop_rec(int_target, int_list, depth = 1):
+def binary_chop_rec(int_target, int_list):
 	""" Does a binary search on a list and 
 		returns index of the targeted element 
 		from the list or -1 if not found
 		
-		This is a decorated recursive functions, thus, the first
+		This is a decorated recursive functions. Thus, the first
 		printed running time is for the last invoked function in the 
 		recursive chain
 
@@ -66,21 +66,62 @@ def binary_chop_rec(int_target, int_list, depth = 1):
 	# recursion stop condition
 	if len(int_list) == 1: 
 		leftover = int_list.pop()
-		print('\ndepth: {}\n'.format(depth))
 		return leftover - 1 if leftover == int_target else not_found_value
-	print('\ndepth: {}'.format(depth))
-	depth += 1
 	
 	list_middle_value = int_list[len(int_list) / 2]
 	if list_middle_value == int_target:
-		print('result: ', end='')
 		return list_middle_value - 1
 	elif list_middle_value < int_target:
-		return  binary_chop_rec(int_target, int_list[len(int_list) / 2 + 1 :], depth)
+		return  binary_chop_rec(int_target, int_list[len(int_list) / 2 + 1 :])
 	elif list_middle_value > int_target:
-		return  binary_chop_rec(int_target, int_list[: len(int_list) / 2 ], depth)
+		return  binary_chop_rec(int_target, int_list[: len(int_list) / 2 ])
 	
 #print(binary_chop_rec(55, 'a_list'))
 #print(binary_chop_rec(55, []))
 #print(binary_chop_rec(2, [1, 2]))
-#print(binary_chop_rec(999, [i for i in xrange(1, 2 ** 10)]))
+#print(binary_chop_rec(999, [i for i in xrange(1, 2 ** 20)]))
+
+
+# 3. approach:    binary_chop - recursive edition - indices carry-on
+@time_me
+def binary_chop_rec_indices(int_target, int_list, start_index=0, end_index=0):
+	""" Does a binary search on a list and 
+		returns index of the targeted element 
+		from the list or -1 if not found
+		
+		This is a decorated recursive functions. Thus, the first
+		printed running time is for the last invoked function in the 
+		recursive chain
+
+	:rtype: int"""
+	
+	not_found_value = -1
+	if type(int_list) not in (list, ):
+		raise TypeError('{} is not list'.format(type(int_list)))
+	if not len(int_list):
+		return not_found_value
+	if not end_index:
+		end_index = len(int_list) - 1
+	
+	# recursion stop condition
+	if start_index == end_index: 
+		if int_list[start_index] == int_target:
+			return start_index
+		return not_found_value
+	
+	arithmetic_mean = (start_index + end_index) / 2
+	list_middle_value = int_list[arithmetic_mean]
+	if list_middle_value == int_target:
+		return arithmetic_mean
+	elif list_middle_value < int_target:
+		return  binary_chop_rec_indices(int_target, int_list, 
+										arithmetic_mean + 1, end_index)
+	elif list_middle_value > int_target:
+		return  binary_chop_rec_indices(int_target, int_list, 
+										start_index, arithmetic_mean - 1)
+	
+#print(binary_chop_rec(55, 'a_list'))
+#print(binary_chop_rec(55, []))
+#print(binary_chop_rec(2, [1, 2]))
+print(binary_chop_rec(999, [i for i in xrange(1, 2 ** 20)]))
+
